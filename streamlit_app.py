86046a8dc73c4764352f12796327be19e9a2e6b8
YassineBenMaktouf <yassine.benmaktouf@esprit.tn>
2023-12-21 23:55:40 +0100

    1

---
+++ streamlit_app.py
@@ -0,0 +1,26 @@
+import openai
+import streamlit as st
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+
+
+st.title("💬 Chatbot")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    openai.api_key =  os.getenv("OPENAI_API_KEY")
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
